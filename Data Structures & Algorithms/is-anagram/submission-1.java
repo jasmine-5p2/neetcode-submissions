e8c15111489abe5d82b543a017a1bf5919e07786
jasmine-5p2 <jasminekollapu@gmail.com>
2026-08-07 12:11:24 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int a[] = new int[26];
+        for(char ch:s.toCharArray()){
+            a[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            a[ch-'a']--;
+        }
+        for(int i:a){
+            if(i!=0){
+                return false;
+            }
+        }
+
+return true;
+    }
+}
