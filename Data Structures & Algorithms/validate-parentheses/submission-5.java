5d5681a7bd55fc641903715e77951b21f6817d08
jasmine-5p2 <jasminekollapu@gmail.com>
2026-08-18 13:57:59 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+      Stack<Character>st = new Stack<>();
+
+     
+    
+
+      for(char ch:s.toCharArray()){
+        if(ch=='('||ch=='{'||ch=='['){
+            st.push(ch);
+
+        }
+        else if(!st.isEmpty()&&((st.peek()=='('&&ch==')')||(st.peek()=='{'&&ch=='}')||(st.peek()=='['&&ch==']'))){
+            st.pop();
+        }
+        else{
+            return false;
+        }
+
+
+
+
+
+      }
+
+      return st.isEmpty();
+
+        
+    }
+}
